aecc781c6d3e39537264bb8eb6dc68cb68891742
David Orbelian <david.orbelian@gmail.com>
2017-09-04 14:33:14 +0400

    Add MicroNetCore.Data solution with MicroNetCore.Data.Abstractions class library project.

---
+++ src/MicroNetCore.Data.Abstractions/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MicroNetCore.Data.Abstractions
+{
+    public interface IRepository<TModel>
+        where TModel : class, new()
+    {
+        Task<IEnumerable<TModel>> FindAsync(Expression<Func<TModel, bool>> predicate = null);
+        Task<TModel> GetAsync(long id);
+        Task PostAsync(TModel model);
+        Task PutAsync(long id, TModel model);
+        Task DeleteAsync(long id);
+    }
+}
