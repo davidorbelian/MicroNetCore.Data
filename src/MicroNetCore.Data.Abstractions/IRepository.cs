3a32582bfaf202ae1dc5a053e6a031aff7d5cfbf
David Orbelian <david.orbelian@gmail.com>
2017-09-06 15:43:42 +0400

    Add FindPageAsync method to IRepository. Update package version.

--- src/MicroNetCore.Data.Abstractions/IRepository.cs
+++ src/MicroNetCore.Data.Abstractions/IRepository.cs
@@ -9,7 +9,13 @@ namespace MicroNetCore.Data.Abstractions
     public interface IRepository<TModel>
         where TModel : class, IModel, new()
     {
-        Task<IEnumerable<TModel>> FindAsync(Expression<Func<TModel, bool>> predicate = null);
+        Task<IEnumerable<TModel>> FindAsync(
+            Expression<Func<TModel, bool>> predicate = null);
+
+        Task<IEnumerable<TModel>> FindPageAsync(
+            int pageIndex, int pageSize,
+            Expression<Func<TModel, bool>> predicate = null);
+
         Task<TModel> GetAsync(long id);
         Task<long> PostAsync(TModel model);
         Task PutAsync(long id, TModel model);
