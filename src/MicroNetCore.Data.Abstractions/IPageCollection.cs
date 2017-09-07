cd639c8bb31f1abe5566a9eac58859c8236f0c76
David Orbelian <david.orbelian@gmail.com>
2017-09-07 21:04:09 +0400

    Add DataMember attributes for IPageCollection. Update package version.

--- src/MicroNetCore.Data.Abstractions/IPageCollection.cs
+++ src/MicroNetCore.Data.Abstractions/IPageCollection.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace MicroNetCore.Data.Abstractions
 {
     public interface IPageCollection<TContent> : ICollection<TContent>
     {
+        [DataMember]
         int PageCount { get; }
+
+        [DataMember]
         int PageIndex { get; }
+
+        [DataMember]
         int PageSize { get; }
     }
 }
