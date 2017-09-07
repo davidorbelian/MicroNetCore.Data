2d844d6608f36a2358677afd18b6ccc366154ff8
David Orbelian <david.orbelian@gmail.com>
2017-09-07 18:53:08 +0400

    Add IRepositoryFactory. Update package version.

---
+++ src/MicroNetCore.Data.Abstractions/IRepositoryFactory.cs
@@ -0,0 +1,10 @@
+﻿using MicroNetCore.Models;
+
+namespace MicroNetCore.Data.Abstractions
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<TModel> Create<TModel>()
+            where TModel : class, IModel, new();
+    }
+}
