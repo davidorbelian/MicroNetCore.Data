2554342555db6cacdd11e2e0b36aa22f8aeac022
David Orbelian <david.orbelian@gmail.com>
2017-09-18 18:57:37 +0400

    Add MicroNetCore.Data.Core class library project.

---
+++ src/MicroNetCore.Data.Core/ConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MicroNetCore.Data.Core
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("Default");
+        }
+    }
+}
