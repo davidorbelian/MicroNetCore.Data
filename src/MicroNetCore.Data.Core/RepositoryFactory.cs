6c823d74b42d35551484a2e9c13026c26f3cfc1b
David Orbelian <david.orbelian@gmail.com>
2017-09-26 17:57:54 +0400

    Add abstract RepositoryFactories.

---
+++ src/MicroNetCore.Data.Core/RepositoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using MicroNetCore.Data.Abstractions.Repositories;
+using MicroNetCore.Models;
+
+namespace MicroNetCore.Data.Core
+{
+    public abstract class RepositoryFactory : IRepositoryFactory
+    {
+        public abstract IRepository<TModel> Create<TModel>()
+            where TModel : class, IModel, new();
+    }
+}
---
+++ src/MicroNetCore.Data.EfCore/EfCoreRepositoryFactory.cs
@@ -0,0 +1,8 @@
+﻿using MicroNetCore.Data.Core;
+
+namespace MicroNetCore.Data.EfCore
+{
+    public abstract class EfCoreRepositoryFactory : RepositoryFactory
+    {
+    }
+}
